b628fd45b1ebf3ee0fbf9c927ae62370a6fc3b2d
crazy frog <77507284+CrazyFrogEpic@users.noreply.github.com>
2021-11-19 22:16:06 -0500

    Make login actually look good
    <hr> reaks

--- web/public/sign_in.php
+++ web/public/sign_in.php
@@ -40,35 +40,29 @@
 			<div id="masthead-container"><?php require($_SERVER['DOCUMENT_ROOT'] . "/s/mod/header.php"); ?></div>
 			<div id="content-container">
 				<!-- begin content -->
-				<div id="content">
-                    <h3>Sign In to SubRocks</h3>
-                    <hr><br>
+				<div id="content" style="">
+                    <h2>Sign In to SubRocks</h2>
+                    <br>
 					<div class="www-left">
 						<b>Join the rockiest video-sharing community!</b><br>
 						Sign up now to get full access with your SubRocks account: <br><br>
 						<ul>
-							<li>&bull; &nbsp; Comment, rate, and make video responses to your favorite videos</li>
-							<li>&bull; &nbsp; Upload and share your videos with millions of other users</li>
-							<li>&bull; &nbsp; Save your favorite videos to watch and share later</li>
-							<li>&bull; &nbsp; Enter your videos into contests for fame and prizes</li>
-						</ul><br><br>
+							<li>• &nbsp; Comment, rate, and make video responses to your favorite videos</li>
+							<li>• &nbsp; Upload and share your videos with millions of other users</li>
+							<li>• &nbsp; Save your favorite videos to watch and share later</li>
+							<li>• &nbsp; Enter your videos into contests for fame and prizes</li>
+						</ul><br><a href="/sign_up">Sign up now </a> to join the SubRocks community.<br>
+<br><br>
 					</div>
 					<div class="www-right">
 						<div class="sign-in-div">
 							<form method="POST" action="/d/login">
-								<b>Username</b><br><br>
-								<input class="yt-uix-form-input-text" type="text" name="username" placeholder="Username"><br><br>
-								<b>Password</b><br><br>
-								<input class="yt-uix-form-input-text" type="password" name="password" placeholder="Password"><br><br>
+								
+								<input type="text" name="username" placeholder="Username" style="" class="yt-uix-form-input-text">
+								
+								<input class="yt-uix-form-input-text" type="password" name="password" placeholder="Password" style="margin-top: 9px;">
 
-								<input class="yt-uix-button yt-uix-button-default" type="submit" value="Login">&nbsp;&nbsp;&nbsp; or &nbsp;&nbsp;&nbsp;
-									<a style="color: white; text-decoration: none;" href="/sign_up">
-									<button type="button" class=" yt-uix-button yt-uix-button-primary yt-uix-button-size-default" href="/sign_up" role="button">
-										<span class="yt-uix-button-content">
-											Sign Up
-										</span>
-									</button>
-								</a>
+								<input class="yt-uix-button yt-uix-button-primary yt-uix-button-size-default" type="submit" value="Login" style=margin-top: 7px;">
 							</form>
 						</div>
 					</div>
