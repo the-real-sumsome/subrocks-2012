53929d2392faab7ce24cd03dd85a3d60f407dc1f
chief bazinga <71347819+the-real-sumsome@users.noreply.github.com>
2021-12-01 23:17:11 -0800

    templating experiemnts

--- web/public/index.php
+++ web/public/index.php
@@ -3,10 +3,13 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/s/classes/time_manip.php"); ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/s/classes/user_helper.php"); ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/s/classes/video_helper.php"); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/s/classes/page_builder.php"); ?>
 <?php $__video_h = new video_helper($__db); ?>
+<?php $__page_b = new page_builder("templates/m"); ?>
 <?php $__user_h = new user_helper($__db); ?>
 <?php $__db_h = new db_helper(); ?>
 <?php $__time_h = new time_helper(); ?>
+<?php ob_start(); ?>
 <?php
 	$__server->page_embeds->page_title = "SubRocks - Homepage";
 	$__server->page_embeds->page_description = "SubRocks is a site dedicated to bring back the 2012 layout of YouTube.";
@@ -289,72 +292,11 @@
 													$video['author'] = htmlspecialchars($video['author']);		
 													$video['title'] = htmlspecialchars($video['title']);
 													$video['description'] = $__video_h->shorten_description($video['description'], 50);
+													$video['pfp'] = $__user_h->fetch_pfp($video['author']);
+
+													echo $__page_b->return_template_replace("index_video_1.module", $video);
+												} 
 											?>
-											<li>
-												<div class="feed-item-container first " data-channel-key="UCR2A9ZNliJfgC66IvIpe-Zw">
-													<div class="feed-author-bubble-container">
-														<a href="/user/<?php echo $video['author']; ?>" class="feed-author-bubble   ">  <span class="feed-item-author">
-														<span class="video-thumb ux-thumb yt-thumb-square-28 "><span class="yt-thumb-clip"><span class="yt-thumb-clip-inner"><img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt="<?php echo $video['author']; ?>" data-thumb="/dynamic/pfp/<?php echo $__user_h->fetch_pfp($video['author']); ?>" width="28"><span class="vertical-align"></span></span></span></span>
-														</span>
-														</a>  
-													</div>
-													<div class="feed-item-main">
-														<div class="feed-item-header">
-															<span class="feed-item-actions-line ">
-															<span class="feed-item-owner">    <a href="/user/<?php echo $video['author']; ?>" class="yt-uix-sessionlink yt-user-name " data-sessionlink="feature=g-logo-xit" dir="ltr"><?php echo $video['author']; ?></a>
-															</span>
-															uploaded a video
-															<span class="feed-item-time">
-															<?php echo $video['age']; ?>
-															</span>
-															</span>
-														</div>
-														<div class="feed-item-content-wrapper clearfix">
-															<div class="feed-item-thumb">
-																<a class="ux-thumb-wrap contains-addto  yt-uix-contextlink yt-uix-sessionlink" data-sessionlink="ei=CNLr3rbS3rICFSwSIQodSW397Q%3D%3D&amp;context=G266dc06FOAAAAAAAAAA" href="/watch?v=<?php echo $video['rid']; ?>">
-																<span class="video-thumb ux-thumb yt-thumb-default-185 "><span class="yt-thumb-clip"><span class="yt-thumb-clip-inner"><img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt="Thumbnail" onerror="this.onerror=null;this.src='/dynamic/thumbs/default.jpg';" data-thumb="/dynamic/thumbs/<?php echo $video['thumbnail']; ?>" width="185"><span class="vertical-align"></span></span></span></span>
-																<span class="video-time"><?php echo $video['duration']; ?></span>
-																<button onclick=";return false;" title="Watch Later" type="button" class="addto-button video-actions addto-watch-later-button-sign-in yt-uix-button yt-uix-button-default yt-uix-button-short yt-uix-tooltip" data-button-menu-id="shared-addto-watch-later-login" data-video-ids="rLHU-_OhT8g" role="button"><span class="yt-uix-button-content">  <img src="//s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt="Watch Later">
-																</span><img class="yt-uix-button-arrow" src="//s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt=""></button>
-																</a>
-															</div>
-															<div class="feed-item-content">
-																<h4>
-																	<a class="feed-video-title title yt-uix-contextlink  yt-uix-sessionlink  secondary" href="/watch?v=<?php echo $video['rid']; ?>" data-sessionlink="ei=CNLr3rbS3rICFSwSIQodSW397Q%3D%3D&amp;feature=g-logo-xit&amp;context=G266dc06FOAAAAAAAAAA">
-																		<?php echo $video['title']; ?>
-																	</a>
-																</h4>
-																<div class="metadata">
-																	<a href="/user/<?php echo $video['author']; ?>" class="yt-user-photo ">
-																	<span class="video-thumb ux-thumb yt-thumb-square-18 "><span class="yt-thumb-clip"><span class="yt-thumb-clip-inner"><img src="http://s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt="<?php echo $video['author']; ?>" data-thumb="/dynamic/pfp/<?php echo $__user_h->fetch_pfp($video['author']); ?>" width="18"><span class="vertical-align"></span></span></span></span>
-																	</a>
-																	<a href="/user/<?php echo $video['author']; ?>" class="yt-uix-sessionlink yt-user-name " data-sessionlink="ei=CNLr3rbS3rICFSwSIQodSW397Q%3D%3D&amp;feature=g-logo-xit" dir="ltr"><?php echo $video['author']; ?></a>
-																	<span class="bull">•</span>
-																	<span class="view-count">
-																	<?php echo $video['views']; ?> views
-																	</span>
-																	<div class="description">
-																		<p><?php echo $video['description']; ?></p>
-																	</div>
-																</div>
-															</div>
-														</div>
-													</div>
-												</div>
-												<div class="feed-item-dismissal-notices">
-													<div class="feed-item-dismissal feed-item-dismissal-hide hid">This item has been hidden</div>
-													<div class="feed-item-dismissal feed-item-dismissal-uploads hid">In the future you will only see uploads from   <span class="feed-item-owner">    <a href="/user/<?php echo $video['author']; ?>" class="yt-uix-sessionlink yt-user-name " data-sessionlink="feature=g-logo-xit" dir="ltr"><?php echo $video['author']; ?></a>
-														</span>
-													</div>
-													<div class="feed-item-dismissal feed-item-dismissal-all-activity hid">In the future you will see all activity from   <span class="feed-item-owner">    <a href="/user/<?php echo $video['author']; ?>" class="yt-uix-sessionlink yt-user-name " data-sessionlink="feature=g-logo-xit" dir="ltr"><?php echo $video['author']; ?></a>
-														</span>
-													</div>
-													<div class="feed-item-dismissal feed-item-dismissal-unsubscribe hid">You have been unsubscribed from   <span class="feed-item-owner">    <a href="/user/<?php echo $video['author']; ?>" class="yt-uix-sessionlink yt-user-name " data-sessionlink="feature=g-logo-xit" dir="ltr"><?php echo $video['author']; ?></a>
-														</span>
-													</div>
-												</div>
-											</li>
-											<?php } ?>
 										</ul>
 									</div>
 								</div>
@@ -624,3 +566,25 @@
 		</script>
 	</body>
 </html>
+<?php
+/* MAKE THIS WAY LESS UGLY.... */
+
+$__template = ob_get_clean();
+$__template_inputs = [
+	'header' => '',
+	'test2' => '',
+	'test3' => '',
+];
+
+foreach($__template_inputs as $key => $input) {
+	$__template = 
+		str_replace(
+			"{{" . $key . "}}", 
+			$input, 
+			$__template
+		);
+}
+
+echo $__template;
+
+?>
